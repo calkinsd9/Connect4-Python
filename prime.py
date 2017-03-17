a2ecde8008aa23eee2c2fe67c71034fee305138b
David Calkins <calkinda@eos24.cis.gvsu.edu>
2017-03-17 11:12:13 -0400

    command line parser now working

--- prime.py
+++ prime.py
@@ -5,10 +5,10 @@ import argparse
 parser = argparse.ArgumentParser(description='Play a game of Connect 4 (or more)')
 
 parser.add_argument('-w', '--width', default=7, type=int, help='an integer that sets the width of the board', metavar='int')
-parser.add_argument('-h', '--height', default=7, type=int, help='an integer that sets the height of the board', metavar='int')
+parser.add_argument('-he', '--height', default=7, type=int, help='an integer that sets the height of the board', metavar='int')
 parser.add_argument('-c', '--connect', default=4, type=int, help='an integer that sets the number of spaces needed to win', metavar='int')
 parser.add_argument('-s', '--square', type=int, help='an integer that sets the width and height of a square board', metavar='int')
 parser.add_argument('-l', '--load', type=str, help='a filename with a save game object to be loaded', metavar='filename')
 
 args = parser.parse_args()
-print vars(args)
+print (vars(args))
