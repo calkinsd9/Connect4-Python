e6fb4944401423079dbcfd1bcb87da4e9d1351e6
David Calkins <calkinsd9@yahoo.com>
2017-03-25 11:32:40 -0400

    pushing fully functional commit before adding computer AI

--- prime.py
+++ prime.py
@@ -4,22 +4,34 @@ import argparse
 import pickle
 import os
 import sys
+from random import randint
 
 #len(gameBoard) == height
 #len(gameBoard[0] == width)
+#gameBoard[YVAL][XVAL]
 
 EMPTYVAL = '.'
 PLAYERVAL = 'x'
 COMPVAL = 'o'
+inGameCommands = ['s', 'l', 'q']
+
 
 def clearScreen():
     #clear screen code taken from  http://stackoverflow.com/questions/2084508/clear-terminal-in-python
-    os.system('cls' if os.name == 'nt' else 'clear')
+    #os.system('cls' if os.name == 'nt' else 'clear')
+    return
+    
+def paddingString(padding):
+    string = ''
+    for i in range(padding):
+        string += ' '
+    return string
 
 def printGameBoard(gameBoard):
     width = len(gameBoard[0])
     height = len(gameBoard)
     padding = 38 - width
+    boardString = paddingString(padding)
     
     print('\n\n')
     print('  _|_|_|                                                      _|      _|  _|')
@@ -28,98 +40,127 @@ def printGameBoard(gameBoard):
     print('_|        _|    _|  _|    _|  _|    _|  _|        _|          _|          _|  ')
     print('_|_|_|_|    _|_|    _|    _|  _|    _|    _|_|_|    _|_|_|    _|          _|\n\n')
     
-    #add padding to board string
-    for i in range(padding):
-        boardString += ' '
+    #column numbers on top
+    for i in range(width):
+        if (i < 10):
+            boardString += ' ' + str(i+1)
+        else:
+            boardString += str(i+1)
+    boardString += '\n' + paddingString(padding) + '|'
     
-    boardString += '|'
+    #game board
     count = 0
     for row in gameBoard:
         for value in row:
             boardString += value + '|'
             count += 1
             if count == len(gameBoard[0]):
-                boardString += '\n|'
+                boardString += '\n' + paddingString(padding) + '|'
                 count =0
     boardString = boardString[:-1]
+    
+    #column numbers on bottom
+    for i in range(width):
+        if (i < 10):
+            boardString += ' ' + str(i+1)
+        else:
+            boardString += str(i+1)
+
     print(boardString)
 
 def resetGameBoard(gameBoard):
     rowIndex = 0
     for row in gameBoard:
         gameBoard[rowIndex] = list(map(lambda x: EMPTYVAL, row))
-        rowIndex += 1
-        
-        
-def validateUserInput(gameBoard, userInput):
-    while True:
-        if (userInput == 'q' or userInput == 's'):
-            return
-        
-        if (isValidMove(gameBoard, userInput)):
-            return
-            
-        print('Column is full. Please choose another.\n\n')
-        userInput = input('\n\nEnter column number where you\'d like to drop your piece\n(Enter \'q\' to quit)\n')
-    
+        rowIndex += 1    
     
 def isValidMove(gameBoard, userInput):
-    iUserInput = int(userInput, 10)
-    if (iUserInput > 0 and iUserInput <= len(gameBoard[0])):
-        if (gameBoard[iUserInput][0] == EMPTYVAL):
-            return True            
+    iUserInput = int(userInput, 10) - 1
+    if (iUserInput >= 0 and iUserInput < len(gameBoard[0])):
+        if (gameBoard[0][iUserInput] == EMPTYVAL):
+            #debug
+            print('Value for valid move is' + str(gameBoard[0][iUserInput]))
+            return True
+    #debug
+    print('Column is full')
     return False
     
 def userMove(userInput, gameBoard):
-    iUserInput = int(userInput)
+    iUserInput = int(userInput) - 1
     height = len(gameBoard)
     
-    for h in range(len(gameBoard)):
-        if (gameboard[iUserInput-1][h] != EMPTYVAL):
+    for h in range(height):
+        if (gameBoard[h][iUserInput] != EMPTYVAL):
+            h -= 1
+            break
+    
+    gameBoard[h][iUserInput] = PLAYERVAL
+    
+def computerMove(gameBoard):
+    height = len(gameBoard)
+    width = len(gameBoard[0])
+    
+    move = randint(1,width)
+    #debug
+    print('Comp move: ' + str(move))
+    while not isValidMove(gameBoard, str(move)):
+        move = randint(1,width)
+    #debug
+    print('Comp move: ' + str(move))
+    for h in range(height):
+        if (gameBoard[h][move-1] != EMPTYVAL):
+            h -= 1
             break
             
-    gameBoard[iUserInput-1][h-1] = PLAYERVAL
+    gameBoard[h][move-1] = COMPVAL
+    
     
 def checkWin(gameBoard, connect, value):
     width = len(gameBoard[0])
     height = len(gameBoard)
-    for h in range(len(gameBoard)):
+    for h in range(height):
         for w in range(width):
-            if (gameBoard[w][h] == value):
+            if (gameBoard[h][w] == value):
 
                 #check horizontal
                 count = 1
                 for i in range(w+1, width):
-                    if (gameBoard[i][h] != value):
+                    if (gameBoard[h][i] != value):
                         break
                     count += 1
                     
                 if (count == connect):
+                    #debug
+                    print('Win by horizontal')
                     return True
                     
                 #check vertical
                 count = 1
                 for j in range(h+1, height):
-                    if (gameBoard[w][j] != value):
+                    if (gameBoard[j][w] != value):
                         break
                     count += 1
 
                 if (count == connect):
+                    #debug
+                    print('Win by vertical')
                     return True
                     
                 #check diagonal left
                 count = 1
                 i = w-1
-                for i in range(h+1, height):
+                for j in range(h+1, height):
                     if (i<0):
                         break
-                    if (gameBoard[i][j] != value):
+                    if (gameBoard[j][i] != value):
                         break
 
                     count += 1
                     i -= 1
 
                 if (count == connect):
+                    #debug
+                    print('Win by diagonal left')
                     return True
                     
                 #check diagonal right
@@ -128,13 +169,15 @@ def checkWin(gameBoard, connect, value):
                 for j in range(h+1, height):
                     if (i >= width):
                         break
-                    if (gameBoard[i][j] != value):
+                    if (gameBoard[j][i] != value):
                         break
                     
                     count += 1
                     i += 1
 
                 if (count == connect):
+                    #debug
+                    print('Win by diagonal right')
                     return True
                     
                     
@@ -147,12 +190,11 @@ def isBoardFull(gameBoard):
     
     for w in range(width):
         for h in range(height):
-            if (gameBoard[h][w] == '.'):
+            if (gameBoard[h][w] == EMPTYVAL):
                 return False
                 
     return True
-    
-    
+        
     
 parser = argparse.ArgumentParser(description='Play a game of Connect 4 (or more)')
 
@@ -174,9 +216,6 @@ filename = args.load
 userWon = False
 computerWon = False
 
-sampleData = {'width': 1, 'height': 2, 'connect': 3}
-pickle.dump(sampleData, open('sampleData', 'wb'))
-
 #if command line parameter is supplied for load, load a game
 if (filename != ''):
     width = 0
@@ -189,7 +228,7 @@ if (filename != ''):
     width = pklData['width']
     height = pklData['height']
     connect = pklData['connect']
-    #TODO load the gameboard
+    gameBoard = pklData['gameBoard']
     
     pklFile.close()
 
@@ -201,7 +240,7 @@ else:
         
     if (connect > width and connect > height):
         clearScreen()
-        print("A " + str(connect) + "-piece long string will not fit on a " + str(width) + " by " + str(height) + " size board. Nobody can win!\n\n\n")
+        print('\n\n\nA ' + str(connect) + '-piece long string will not fit on a ' + str(width) + ' by ' + str(height) + ' size board. Nobody can win!\n\n\n')
         sys.exit()
         
     #set the gameboard
@@ -221,12 +260,29 @@ while userInput != 'n':
         
     printGameBoard(gameBoard)
     
-    userInput = input('\n\nEnter column number where you\'d like to drop your piece\n(Enter \'q\' to quit)\n')
+    userInput = input('\n\nEnter column number where you\'d like to drop your piece\n  (Enter \'q\' to quit, \'s\' to save, or \'l\' to load)\n')
     
-    print('len(gameBoard) = ' + str(len(gameBoard)))
-    print('len(gameBoard[0]) = ' + str(len(gameBoard[0])))
-    print('len(gameBoard[0][0])' + str(len(gameBoard[0][0])))
-    validateUserInput(gameBoard, userInput)
+    invalidInput = True
+    
+    #while loop ensures valid user input 
+    while invalidInput:
+        inputIsCommand = userInput in inGameCommands
+        
+        inputIsInt = True
+        try:
+            int(userInput, 10)
+        except ValueError:
+            inputIsInt = False
+                            
+        if inputIsInt:
+            #debug
+            print('inputIsInt')
+            invalidInput = not isValidMove(gameBoard, userInput)
+        else:
+            invalidInput = not inputIsCommand
+
+        if invalidInput:
+            userInput = input('Input invalid or column is full. Please make another selection.\n\n')
     
     if (userInput == 'q'):
         sys.exit()
@@ -236,12 +292,34 @@ while userInput != 'n':
         pickle.dump(gameData, open('gameData', 'wb'))
         userInput = input('\n\nYour game has been saved in the file gameData\n\nType \'c\' to continue...')
         continue
-
+        
+    if (userInput == 'l'):
+        width = 0
+        height = 0
+        connect = 0
+        square = 0
+        userWon = False
+        computerWon = False
+        
+        pklFile = open('gameData', 'rb')
+        pklData = pickle.load(pklFile)
+        width = pklData['width']
+        height = pklData['height']
+        connect = pklData['connect']
+        gameBoard = pklData['gameBoard']
+        
+        pklFile.close()
+        userInput = input('\n\nYour game has been loaded from the gameData file. Type \'c\' to continue...')
+        continue
+        
     userMove(userInput, gameBoard)
     
-    computerWon = checkWin(gameBoard, connect, COMPVAL)
     userWon = checkWin(gameBoard, connect, PLAYERVAL)
     
+    if not isBoardFull(gameBoard) and not userWon:
+        computerMove(gameBoard)
+        computerWon = checkWin(gameBoard, connect, COMPVAL)
+        
     if userWon:
         clearScreen()
         printGameBoard(gameBoard)
